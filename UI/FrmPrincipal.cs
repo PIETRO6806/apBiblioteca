b1140a0ec2d60e0101a2436aba0c852ece187559
PIETRO6806 <pietro060806@gmail.com>
2022-12-07 18:24:19 -0300

    Pág. 236

--- UI/FrmPrincipal.cs
+++ UI/FrmPrincipal.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace apBiblioteca_22132_22148
+namespace apBiblioteca_22132_22148.UI
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincipal : Form
     {
-        public Form1()
+        public FrmPrincipal()
         {
             InitializeComponent();
         }
